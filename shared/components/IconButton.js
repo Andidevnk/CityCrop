b508daa231685bcfa0e6296f1f1c523d3f1315c6
Panagiotis Plytas <panagiotis.plytas@gmail.com>
2021-01-01 03:52:04 +0200

    Replace TouchableOpacity with Pressable in IconButton

--- shared/components/IconButton.js
+++ shared/components/IconButton.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { Pressable } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
+const defaultRippleConfig = {
+  color: 'rgba(0, 0, 0, .32)',
+  borderless: true,
+};
+const defaultHitSlop = 20; // Extra area in which touch is detected
+
 const IconButton = ({ iconStyle, name, size, color, ...restProps }) => (
-  <TouchableOpacity {...restProps}>
+  <Pressable
+    android_ripple={defaultRippleConfig}
+    hitSlop={defaultHitSlop}
+    {...restProps}
+  >
     <Ionicons style={iconStyle} name={name} size={size} color={color} />
-  </TouchableOpacity>
+  </Pressable>
 );
 
 export default IconButton;
