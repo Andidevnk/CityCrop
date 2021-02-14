c79e15e6ebf9f048c7bcb969e82734bc11509045
Panagiotis Plytas <panagiotis.plytas@gmail.com>
2021-02-14 23:07:55 +0200

    Add ScalableImage component

---
+++ shared/components/ScalableImage.js
@@ -0,0 +1,37 @@
+import React, { useMemo } from 'react';
+import { Image } from 'react-native';
+
+const ScalableImage = ({ style, source, ...restProps }) => {
+  const extraStyle = useMemo(() => {
+    const { width, height } = Image.resolveAssetSource(source);
+    const aspectRatio = width / height;
+
+    let extraStyle;
+    if (style.width && !style.height) {
+      extraStyle = {
+        height: style.width / aspectRatio,
+      };
+    } else if (style.height && !style.width) {
+      extraStyle = {
+        width: style.height * aspectRatio,
+      };
+    } else {
+      extraStyle = {};
+    }
+
+    return extraStyle;
+  }, [style, source]);
+
+  return (
+    <Image
+      style={{
+        ...style,
+        ...extraStyle,
+      }}
+      source={source}
+      {...restProps}
+    />
+  );
+};
+
+export default ScalableImage;
