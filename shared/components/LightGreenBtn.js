c6bd5cc13fd125e07068ca72fb269348855b3503
Panagiotis Plytas <panagiotis.plytas@gmail.com>
2021-02-21 19:59:54 +0200

    Add LightGreenBtn and TextStyles

---
+++ shared/components/LightGreenBtn.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, TouchableHighlight, Text } from 'react-native';
+
+function LightGreenBtn({ style, title, ...restProps }) {
+  return (
+    <TouchableHighlight
+      style={{
+        ...styles.button,
+        ...style,
+      }}
+      activeOpacity={0.9}
+      underlayColor="#4bad01"
+      {...restProps}
+    >
+      <Text style={styles.text}>{title}</Text>
+    </TouchableHighlight>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: '90%',
+    padding: 16,
+    backgroundColor: '#59C901',
+    borderRadius: 27,
+    alignSelf: 'center',
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 18,
+    color: '#FFFFFF',
+  },
+});
+
+export default LightGreenBtn;
