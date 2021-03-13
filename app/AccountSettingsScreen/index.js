2e7fe70a8123cdf751afb1ec4338e2bdfc0e4ead
Panagiotis Plytas <panagiotis.plytas@gmail.com>
2021-03-14 01:20:43 +0200

    Add AccountSettingsScreen and NotificationsScreen dummy components

---
+++ app/AccountSettingsScreen/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const AccountSettingsScreen = () => {
+  return (
+    <View>
+      <Text>AccountSettingsScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default AccountSettingsScreen;
