2bbe4ac21ef5ce13f18f42f3672be8a7348f8fd0
Panagiotis Plytas <panagiotis.plytas@gmail.com>
2021-04-17 00:08:19 +0300

    Add header back button to AccountSettingsScreen

--- app/AccountSettingsNavigator.js
+++ app/AccountSettingsNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   createStackNavigator,
   TransitionPresets,
+  HeaderBackButton,
 } from '@react-navigation/stack';
 
 import { ScreenOptions } from 'shared/constants';
@@ -9,11 +10,18 @@ import AccountSettingsScreen from './drawer/AccountSettingsScreen';
 
 const Stack = createStackNavigator();
 
-const AccountSettingsNavigator = () => {
+const AccountSettingsNavigator = ({ navigation }) => {
   return (
     <Stack.Navigator
       screenOptions={{
         ...TransitionPresets.SlideFromRightIOS,
+        // eslint-disable-next-line react/display-name
+        headerLeft: (props) => (
+          <HeaderBackButton
+            {...props}
+            onPress={() => navigation.navigate('Home')}
+          />
+        ),
       }}
     >
       <Stack.Screen
