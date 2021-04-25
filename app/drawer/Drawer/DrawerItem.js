328d45844f1df83a2a30926ef44ce5015696d3e5
Panagiotis Plytas <panagiotis.plytas@gmail.com>
2021-04-25 18:45:03 +0300

    Move DrawerItem styles to StyleSheet

--- app/drawer/Drawer/DrawerItem.js
+++ app/drawer/Drawer/DrawerItem.js
@@ -6,16 +6,8 @@ import ScalableImage from 'shared/components/ScalableImage';
 const DrawerItem = ({ label, icon, ...restProps }) => {
   return (
     <Pressable style={styles.drawerItem} {...restProps}>
-      <ScalableImage
-        style={{
-          width: 15,
-          marginRight: 15,
-          marginLeft: 5,
-        }}
-        source={icon}
-        resizeMode="contain"
-      />
-      <Text style={{ fontSize: 15, color: '#18191F' }}>{label}</Text>
+      <ScalableImage style={styles.icon} source={icon} resizeMode="contain" />
+      <Text style={styles.label}>{label}</Text>
     </Pressable>
   );
 };
@@ -27,6 +19,15 @@ const styles = StyleSheet.create({
     paddingVertical: 20,
     borderBottomWidth: 1,
     borderBottomColor: 'rgba(190, 190, 190, 0.5)',
+  },
+  icon: {
+    width: 15,
+    marginRight: 15,
+    marginLeft: 5,
+  },
+  label: {
+    fontSize: 15,
+    color: '#18191F',
   },
 });
 
