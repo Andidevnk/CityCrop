4bf2208115507d2f55b72c399ae50ac21e16c13d
Panagiotis Plytas <panagiotis.plytas@gmail.com>
2021-05-02 00:09:27 +0300

    Add new avatar placeholder iage

--- app/drawer/Drawer/UserInfo.js
+++ app/drawer/Drawer/UserInfo.js
@@ -5,13 +5,10 @@ const UserInfo = ({ name }) => {
   return (
     <>
       <Image
-        style={{
-          width: 60,
-          height: 60,
-          marginRight: 15,
-        }}
-        source={require('assets/imgs/user-profile/user-profile.png')}
-        resizeMode="contain"
+        style={styles.userImage}
+        source={require('assets/imgs/user-profile/avatar-placeholder.png')}
+        borderRadius={50 / 2} // Android fix
+        resizeMode="cover" // Android fix
       />
       <Text style={styles.userInfoText} numberOfLines={1}>
         {name}
@@ -21,6 +18,14 @@ const UserInfo = ({ name }) => {
 };
 
 const styles = StyleSheet.create({
+  userImage: {
+    width: 50,
+    height: 50,
+    marginRight: 12,
+    borderWidth: 1.5,
+    borderColor: '#0B7B03',
+    borderRadius: 50 / 2,
+  },
   userInfoText: {
     flex: 1,
     fontSize: 14,
