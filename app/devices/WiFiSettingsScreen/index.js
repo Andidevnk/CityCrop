53ad7ceaf3ef97cd85439706cbee1e5b94a61b59
Panagiotis Plytas <panagiotis.plytas@gmail.com>
2021-05-02 19:48:26 +0300

    Make WiFiSettingsScreen functional

--- app/devices/WiFiSettingsScreen/index.js
+++ app/devices/WiFiSettingsScreen/index.js
@@ -1,41 +1,124 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import {
   StyleSheet,
   View,
   Text,
   TextInput,
-  TouchableOpacity,
+  Pressable,
+  TouchableHighlight,
 } from 'react-native';
-import { Feather } from '@expo/vector-icons';
+import { useDispatch } from 'react-redux';
 
-const WiFiSettingsScreen = () => (
-  <View style={styles.container}>
-    <Text style={styles.title}>Change WiFi settings</Text>
-    <TextInput
-      style={[styles.input, { marginBottom: 15 }]}
-      placeholder="SSID"
-    />
-    <TextInput style={styles.input} placeholder="Password" />
-    <TouchableOpacity
-      style={styles.refreshBtn}
-      onPress={() => console.log('refresh')}
-    >
-      <Feather
-        style={styles.refreshBtnIcon}
-        name="refresh-cw"
-        size={24}
-        color="#59C901"
+import useFormState from 'shared/hooks/useFormState';
+import {
+  connectToNetworkAsync,
+  listNetworksAsync,
+} from 'shared/store/networks/actions';
+import ListModal from 'shared/components/ListModal';
+import ScalableImage from 'shared/components/ScalableImage';
+import LightGreenBtn from 'shared/components/LightGreenBtn';
+
+const WiFiSettingsScreen = ({ navigation }) => {
+  const [form, setForm] = useFormState({
+    network: '',
+    password: '',
+  });
+  const [isNetworksModalVisible, setIsNetworksModalVisible] = useState(false);
+  const [networks, setNetworks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const dispatch = useDispatch();
+  const getNetworks = useCallback(
+    () => dispatch(listNetworksAsync()).then(({ data }) => setNetworks(data)),
+    [dispatch]
+  );
+  const closeNetworksModal = () => setIsNetworksModalVisible(false);
+  const connectToNetwork = () => {
+    setIsLoading(true);
+    dispatch(connectToNetworkAsync(form.network, form.password)).then(() => {
+      setIsLoading(false);
+      navigation.navigate('Devices');
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.title}>Change WiFi settings</Text>
+        <Text style={{ fontSize: 14, color: '#18191F' }}>
+          First connect to the{' '}
+          <Text style={{ fontWeight: 'bold' }}>CityCrop</Text> WiFi network and
+          then press <Text style={{ fontWeight: 'bold' }}>Refresh</Text> to get
+          the available networks.
+        </Text>
+
+        <View style={{ marginVertical: 40 }}>
+          <Pressable
+            style={{ marginBottom: 20 }}
+            onPress={() => setIsNetworksModalVisible(true)}
+          >
+            <TextInput
+              style={styles.input}
+              placeholder="WiFi Network"
+              value={form.network}
+              pointerEvents="none"
+              editable={false}
+            />
+          </Pressable>
+          <ListModal
+            visible={isNetworksModalVisible}
+            data={networks}
+            keyExtractor={(item) => item.ssid}
+            renderItemContent={(item) => item.ssid}
+            onItemPress={(network) => {
+              setForm({ network: network.ssid });
+              closeNetworksModal();
+            }}
+            onOutsidePress={closeNetworksModal}
+            onRequestClose={closeNetworksModal} // Back button press
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            autoCompleteType="password"
+            secureTextEntry={true}
+            value={form.password}
+            onChangeText={(text) => setForm({ password: text })}
+          />
+        </View>
+
+        <TouchableHighlight
+          style={{
+            flexDirection: 'row',
+            alignSelf: 'center',
+            alignItems: 'center',
+            padding: 20,
+            borderWidth: 1.5,
+            borderRadius: 15,
+            borderColor: '#0B7B03',
+          }}
+          underlayColor="#EEEEEE"
+          onPress={getNetworks}
+        >
+          <>
+            <ScalableImage
+              style={{ height: 20, marginRight: 10 }}
+              source={require('assets/icons/refresh.png')}
+            />
+            <Text style={{ fontSize: 16, color: '#0B7B03' }}>Refresh</Text>
+          </>
+        </TouchableHighlight>
+      </View>
+
+      <LightGreenBtn
+        style={{ marginTop: 30 }}
+        title="Connect"
+        loading={isLoading}
+        onPress={connectToNetwork}
       />
-      <Text style={styles.refreshBtnText}>Refresh</Text>
-    </TouchableOpacity>
-    <TouchableOpacity
-      style={styles.saveBtn}
-      onPress={() => console.log('save')}
-    >
-      <Text style={styles.saveBtnText}>Save</Text>
-    </TouchableOpacity>
-  </View>
-);
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -44,9 +127,10 @@ const styles = StyleSheet.create({
     paddingBottom: 30,
     paddingHorizontal: 20,
     backgroundColor: '#FFFFFF',
+    justifyContent: 'space-between',
   },
   title: {
-    marginBottom: 30,
+    marginBottom: 20,
     fontSize: 20,
     fontWeight: 'bold',
     color: '#18191F',
@@ -63,40 +147,6 @@ const styles = StyleSheet.create({
       height: 1,
     },
     shadowOpacity: 0.1,
-  },
-  refreshBtn: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 20,
-    paddingHorizontal: 25,
-    backgroundColor: '#FFFFFF',
-    borderRadius: 15,
-    borderWidth: 1.5,
-    borderColor: '#0B7B03',
-    marginTop: 60,
-  },
-  refreshBtnIcon: {
-    marginRight: 16,
-  },
-  refreshBtnText: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#0B7B03',
-  },
-  saveBtn: {
-    width: '90%',
-    marginTop: 'auto',
-    alignSelf: 'center',
-    alignItems: 'center',
-    paddingVertical: 16,
-    paddingHorizontal: 80,
-    backgroundColor: '#59C901',
-    borderRadius: 27,
-  },
-  saveBtnText: {
-    fontSize: 18,
-    color: '#FFFFFF',
   },
 });
 
