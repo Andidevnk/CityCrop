4ed05a1c55bb96c6372a78ff89b1d37df674d5c7
Panagiotis Plytas <panagiotis.plytas@gmail.com>
2021-05-02 19:50:22 +0300

    Add KeyboardDismissArea in WiFiSettingsScreen

--- app/devices/WiFiSettingsScreen/index.js
+++ app/devices/WiFiSettingsScreen/index.js
@@ -17,6 +17,7 @@ import {
 import ListModal from 'shared/components/ListModal';
 import ScalableImage from 'shared/components/ScalableImage';
 import LightGreenBtn from 'shared/components/LightGreenBtn';
+import KeyboardDismissArea from 'shared/components/KeyboardDismissArea';
 
 const WiFiSettingsScreen = ({ navigation }) => {
   const [form, setForm] = useFormState({
@@ -42,81 +43,83 @@ const WiFiSettingsScreen = ({ navigation }) => {
   };
 
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.title}>Change WiFi settings</Text>
-        <Text style={{ fontSize: 14, color: '#18191F' }}>
-          First connect to the{' '}
-          <Text style={{ fontWeight: 'bold' }}>CityCrop</Text> WiFi network and
-          then press <Text style={{ fontWeight: 'bold' }}>Refresh</Text> to get
-          the available networks.
-        </Text>
+    <KeyboardDismissArea>
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.title}>Change WiFi settings</Text>
+          <Text style={{ fontSize: 14, color: '#18191F' }}>
+            First connect to the{' '}
+            <Text style={{ fontWeight: 'bold' }}>CityCrop</Text> WiFi network
+            and then press <Text style={{ fontWeight: 'bold' }}>Refresh</Text>{' '}
+            to get the available networks.
+          </Text>
 
-        <View style={{ marginVertical: 40 }}>
-          <Pressable
-            style={{ marginBottom: 20 }}
-            onPress={() => setIsNetworksModalVisible(true)}
-          >
+          <View style={{ marginVertical: 40 }}>
+            <Pressable
+              style={{ marginBottom: 20 }}
+              onPress={() => setIsNetworksModalVisible(true)}
+            >
+              <TextInput
+                style={styles.input}
+                placeholder="WiFi Network"
+                value={form.network}
+                pointerEvents="none"
+                editable={false}
+              />
+            </Pressable>
+            <ListModal
+              visible={isNetworksModalVisible}
+              data={networks}
+              keyExtractor={(item) => item.ssid}
+              renderItemContent={(item) => item.ssid}
+              onItemPress={(network) => {
+                setForm({ network: network.ssid });
+                closeNetworksModal();
+              }}
+              onOutsidePress={closeNetworksModal}
+              onRequestClose={closeNetworksModal} // Back button press
+            />
             <TextInput
               style={styles.input}
-              placeholder="WiFi Network"
-              value={form.network}
-              pointerEvents="none"
-              editable={false}
+              placeholder="Password"
+              autoCompleteType="password"
+              secureTextEntry={true}
+              value={form.password}
+              onChangeText={(text) => setForm({ password: text })}
             />
-          </Pressable>
-          <ListModal
-            visible={isNetworksModalVisible}
-            data={networks}
-            keyExtractor={(item) => item.ssid}
-            renderItemContent={(item) => item.ssid}
-            onItemPress={(network) => {
-              setForm({ network: network.ssid });
-              closeNetworksModal();
+          </View>
+
+          <TouchableHighlight
+            style={{
+              flexDirection: 'row',
+              alignSelf: 'center',
+              alignItems: 'center',
+              padding: 20,
+              borderWidth: 1.5,
+              borderRadius: 15,
+              borderColor: '#0B7B03',
             }}
-            onOutsidePress={closeNetworksModal}
-            onRequestClose={closeNetworksModal} // Back button press
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Password"
-            autoCompleteType="password"
-            secureTextEntry={true}
-            value={form.password}
-            onChangeText={(text) => setForm({ password: text })}
-          />
+            underlayColor="#EEEEEE"
+            onPress={getNetworks}
+          >
+            <>
+              <ScalableImage
+                style={{ height: 20, marginRight: 10 }}
+                source={require('assets/icons/refresh.png')}
+              />
+              <Text style={{ fontSize: 16, color: '#0B7B03' }}>Refresh</Text>
+            </>
+          </TouchableHighlight>
         </View>
 
-        <TouchableHighlight
-          style={{
-            flexDirection: 'row',
-            alignSelf: 'center',
-            alignItems: 'center',
-            padding: 20,
-            borderWidth: 1.5,
-            borderRadius: 15,
-            borderColor: '#0B7B03',
-          }}
-          underlayColor="#EEEEEE"
-          onPress={getNetworks}
-        >
-          <>
-            <ScalableImage
-              style={{ height: 20, marginRight: 10 }}
-              source={require('assets/icons/refresh.png')}
-            />
-            <Text style={{ fontSize: 16, color: '#0B7B03' }}>Refresh</Text>
-          </>
-        </TouchableHighlight>
+        <LightGreenBtn
+          style={{ marginTop: 30 }}
+          title="Connect"
+          loading={isLoading}
+          onPress={connectToNetwork}
+        />
       </View>
-
-      <LightGreenBtn
-        style={{ marginTop: 30 }}
-        title="Connect"
-        loading={isLoading}
-        onPress={connectToNetwork}
-      />
-    </View>
+    </KeyboardDismissArea>
   );
 };
 
