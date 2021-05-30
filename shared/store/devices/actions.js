a5d0fe104a16f0de857b4671341e5a33bc707a42
Panagiotis Plytas <panagiotis.plytas@gmail.com>
2021-05-30 16:47:24 +0300

    Format devices actions

--- shared/store/devices/actions.js
+++ shared/store/devices/actions.js
@@ -2,22 +2,21 @@ import AuthAxios from 'shared/axios/AuthAxios';
 
 export const SET_DEVICES = 'SET_DEVICES';
 export const UPDATE_DEVICE_MEASUREMENTS = 'UPDATE_DEVICE_MEASUREMENTS';
+export const SET_NEW_DEVICE = 'SET_NEW_DEVICE';
 
-export const loadDevicesAsync = () => (dispatch) => {
-  return AuthAxios.get(
-    'https://api.citycrop.io/v1/devices/all'
-  ).then((response) => dispatch(setDevices(response.data.devices)));
-};
+export const loadDevicesAsync = () => (dispatch) =>
+  AuthAxios.get('https://api.citycrop.io/v1/devices/all').then((response) =>
+    dispatch(setDevices(response.data.devices))
+  );
 
 export const addDeviceAsync = (name, timezone) => (dispatch) => {
   const data = {
     name: name,
     timezone: timezone,
   };
-  return AuthAxios.post(
-    'https://api.citycrop.io/v1/devices/add',
-    data
-  ).then(() => dispatch(loadDevicesAsync()));
+  return AuthAxios.post('https://api.citycrop.io/v1/devices/add', data).then(
+    () => dispatch(loadDevicesAsync())
+  );
 };
 
 export const updateDeviceAsync = (id, name, timezone) => (dispatch) => {
@@ -26,20 +25,18 @@ export const updateDeviceAsync = (id, name, timezone) => (dispatch) => {
     name: name,
     timezone: timezone,
   };
-  return AuthAxios.post(
-    'https://api.citycrop.io/v1/devices/update',
-    data
-  ).then(() => dispatch(loadDevicesAsync()));
+  return AuthAxios.post('https://api.citycrop.io/v1/devices/update', data).then(
+    () => dispatch(loadDevicesAsync())
+  );
 };
 
 export const deleteDeviceAsync = (id) => (dispatch) => {
   const data = {
     device_id: id,
   };
-  return AuthAxios.post(
-    'https://api.citycrop.io/v1/devices/delete',
-    data
-  ).then(() => dispatch(loadDevicesAsync()));
+  return AuthAxios.post('https://api.citycrop.io/v1/devices/delete', data).then(
+    () => dispatch(loadDevicesAsync())
+  );
 };
 
 export const setDevices = (devices) => ({
