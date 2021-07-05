ddedd838575719fdcd128eb54e75939bb11ef6f4
Panagiotis Plytas <panagiotis.plytas@gmail.com>
2021-07-05 23:35:26 +0300

    Add KeyboardAvoidingScrollView component

---
+++ shared/components/KeyboardAvoidingScrollView.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+  StyleSheet,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+  StatusBar,
+} from 'react-native';
+import { useHeaderHeight } from '@react-navigation/stack';
+
+const KeyboardAvoidingScrollView = ({
+  contentContainerStyle,
+  style,
+  children,
+  ...props
+}) => {
+  const headerHeight = useHeaderHeight();
+
+  const behavior = Platform.OS === 'ios' ? 'padding' : 'height';
+  const keyboardVerticalOffset =
+    Platform.OS === 'ios'
+      ? headerHeight + StatusBar.currentHeight
+      : headerHeight;
+
+  return (
+    <KeyboardAvoidingView
+      style={styles.keyboardAvoidingView}
+      behavior={behavior}
+      keyboardVerticalOffset={keyboardVerticalOffset}
+    >
+      <ScrollView
+        style={style}
+        contentContainerStyle={[
+          styles.scrollViewContentContainer,
+          contentContainerStyle,
+        ]}
+        bounces={false}
+        {...props}
+      >
+        {children}
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  keyboardAvoidingView: {
+    flex: 1,
+  },
+  scrollViewContentContainer: {
+    flexGrow: 1,
+  },
+});
+
+export default KeyboardAvoidingScrollView;
