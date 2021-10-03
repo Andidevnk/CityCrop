f45a7c327d73bd5bc9f6d72aee46c8db031bf966
Panagiotis Plytas <panagiotis.plytas@gmail.com>
2021-10-03 19:13:58 +0300

    Add extra device and module status images

--- shared/constants/deviceModuleImages.js
+++ shared/constants/deviceModuleImages.js
@@ -1,10 +1,15 @@
 export const DEVICE_MODULE_IMAGES = {
   'LU-add-module': require('assets/imgs/devices-modules/LU-add-module.png'),
   'UU-add-module': require('assets/imgs/devices-modules/UU-add-module.png'),
+  'LU-offline': require('assets/imgs/devices-modules/LU-offline.png'),
   'LU-online-no-plants': require('assets/imgs/devices-modules/LU-online-no-plants.png'),
   'LU-online-with-plants': require('assets/imgs/devices-modules/LU-online-with-plants.png'),
+  'UU-offline': require('assets/imgs/devices-modules/UU-offline.png'),
   'UU-online-no-plants': require('assets/imgs/devices-modules/UU-online-no-plants.png'),
   'UU-online-with-plants': require('assets/imgs/devices-modules/UU-online-with-plants.png'),
+  'LU-UU-offline': require('assets/imgs/devices-modules/LU-UU-offline.png'),
   'LU-UU-online-no-plants': require('assets/imgs/devices-modules/LU-UU-online-no-plants.png'),
   'LU-UU-online-with-plants': require('assets/imgs/devices-modules/LU-UU-online-with-plants.png'),
+  'LU-UU-connecting': require('assets/imgs/devices-modules/LU-UU-connecting.png'),
+  'LU-UU-wifi': require('assets/imgs/devices-modules/LU-UU-wifi.png'),
 };
